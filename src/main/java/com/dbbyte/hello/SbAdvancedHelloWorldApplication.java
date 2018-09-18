6a3ce020221db28a13f797ac701261c6b8b18cda
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2018-09-18 09:51:27 +0300

    first

---
+++ src/main/java/com/dbbyte/hello/SbAdvancedHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.dbbyte.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAdvancedHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAdvancedHelloWorldApplication.class, args);
+	}
+}
